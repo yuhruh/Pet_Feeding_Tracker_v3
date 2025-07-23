b4e6f34f1e42d344bcc0fdbff82ce3bd62436cc9
Rita Huang <yuhruhgrape@gmail.com>
2025-07-23 12:12:12 +0800

    modify render deploy setting

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
+
 set -o errexit
+
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-# If you have a paid instance type, we recommend moving
-# database migrations like this one from the build command
-# to the pre-deploy command:
-bundle exec rails db:migrate
+bin/rails assets:precompile
+bin/rails assets:clean
+
+bin/rails db:migrate

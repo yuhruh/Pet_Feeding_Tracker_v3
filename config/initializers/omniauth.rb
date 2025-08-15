d4f0ea97cb9aa389b6d2665738a879c78c4e1520
Rita Huang <yuhruhgrape@gmail.com>
2025-08-15 16:13:46 +0800

    update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
-  provider :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret), scope: "email, profile"
+
+  if Rails.env.production?
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: "email, profile"
+  else
+    provider :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret), scope: "email, profile"
+  end
 
   OmniAuth.config.on_failure = Proc.new do |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure

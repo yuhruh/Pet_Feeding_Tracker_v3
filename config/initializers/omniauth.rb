b13acf57f416e18e4dc88dfdfb4ab2d894b7f98a
Rita Huang <yuhruhgrape@gmail.com>
2025-08-15 21:05:12 +0800

    fix syntax error in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
 
   if Rails.env.production?
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: "email, profile", {
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+      :scope => "email, profile",
       :callback_path => '/auth/google_oauth2/callback',
       :request_path => '/auth/google_oauth2'
     }
